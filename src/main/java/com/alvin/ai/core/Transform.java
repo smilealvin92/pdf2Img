c6a1a00e4a122a99c17408e3d3af35a9b7a7fcaa
smilealvin92 <48876213+smilealvin92@users.noreply.github.com>
2021-12-14 22:57:08 +0800

    Add files via upload

---
+++ src/main/java/com/alvin/ai/core/Transform.java
@@ -0,0 +1,179 @@
+package com.alvin.ai.core;
+
+import com.artifex.mupdf.fitz.*;
+import com.alvin.ai.example.Viewer;
+import com.alvin.ai.utils.PdfUtils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @ClassName Transform
+ * @Description TODO
+ * @Author Yang Jianren
+ * @Date 2021/12/13 上午10:45
+ * @Version 1.0
+ */
+public class Transform {
+    protected static int zoomLevel = 10;
+    protected static int rotate = 0;
+    protected static boolean currentICC = true;
+    protected static int currentAA = 8;
+    protected static boolean currentInvert = false;
+    protected static boolean currentTint = false;
+    protected static int tintBlack = 0x303030;
+    protected static int tintWhite = 0xFFFFF0;
+    protected static final int[] zoomList = {
+            18, 24, 36, 54, 72, 96, 120, 144, 180, 216, 288
+    };
+    protected static Matrix pageCTM = new Matrix().scale(zoomList[zoomLevel] / 72.0f * 1.0f);
+
+    public Transform() {
+    }
+
+    public static BufferedImage turnPage2Img(Page page) {
+        Matrix trm = new Matrix(pageCTM).rotate(rotate);
+        Rect bbox = page.getBounds().transform(trm);
+        Pixmap pixmap = new Pixmap(ColorSpace.DeviceBGR, bbox, true);
+        pixmap.clear(255);
+        if (currentICC)
+            Context.enableICC();
+        else
+            Context.disableICC();
+        Context.setAntiAliasLevel(currentAA);
+        DrawDevice dev = new DrawDevice(pixmap);
+        page.run(dev, trm, null);
+        dev.close();
+        dev.destroy();
+        if (currentInvert) {
+            pixmap.invertLuminance();
+            pixmap.gamma(1 / 1.4f);
+        }
+        if (currentTint) {
+            pixmap.tint(tintBlack, tintWhite);
+        }
+        BufferedImage image = PdfUtils.imageFromPixmap(pixmap);
+        pixmap.destroy();
+        return image;
+    }
+
+    public static void init(String muPdfSoAbsPath){
+        try {
+            addLibraryPath(muPdfSoAbsPath);
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("初始化so文件路径失败，程序退出");
+            System.exit(1);
+        }
+    }
+
+    public static List<BufferedImage> turnPdf2Imgs(String pdfAbsPath){
+        List<BufferedImage> res = new ArrayList<>();
+        final String acceleratorPath = PdfUtils.getAcceleratorPath(pdfAbsPath);
+        Document document;
+        try {
+            if (PdfUtils.acceleratorValid(pdfAbsPath, acceleratorPath)) {
+                document = Document.openDocument(pdfAbsPath, acceleratorPath);
+            } else {
+                document = Document.openDocument(pdfAbsPath);
+            }
+            document.saveAccelerator(acceleratorPath);
+            int pageCount = document.countPages();
+            for (int i = 0; i < pageCount; i++){
+                Page page = document.loadPage(i);
+                res.add(Transform.turnPage2Img(page));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println(pdfAbsPath);
+            res = new ArrayList<>();
+        }
+        return res;
+    }
+
+    /**
+     * Adds the specified path to the java library path
+     *
+     * @param pathToAdd the path to add
+     * @throws Exception
+     */
+    public static void addLibraryPath(String pathToAdd) throws Exception{
+        final Field usrPathsField = ClassLoader.class.getDeclaredField("usr_paths");
+        usrPathsField.setAccessible(true);
+
+        //get array of paths
+        final String[] paths = (String[])usrPathsField.get(null);
+
+        //check if the path to add is already present
+        for(String path : paths) {
+            if(path.equals(pathToAdd)) {
+                return;
+            }
+        }
+
+        //add the new path
+        final String[] newPaths = Arrays.copyOf(paths, paths.length + 1);
+        newPaths[newPaths.length-1] = pathToAdd;
+        usrPathsField.set(null, newPaths);
+    }
+
+    public static void traverse(File file, List<String> allPdfPath) {
+        File[] files = file.listFiles();
+        if (files == null) {
+            return;
+        }
+        for (File file1 : files) {
+            if (file1.isDirectory()) {
+                traverse(file1, allPdfPath);
+            } else if (file1.isFile()) {
+                allPdfPath.add(file1.getAbsolutePath());
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+//        String pdfPath = "/home/alvin/yjrwork/bankStreamReco/纯文本类型的银行流水pdf/梁月顺 工行 6222034000018587612.pdf";
+//        String pdfPath = "/home/alvin/yjrwork/bankStreamReco/纯文本类型的银行流水pdf/梁月顺 浦发 6217921101199498.pdf";
+        addLibraryPath("/home/alvin/下载/开源代码/mupdf-1.19.0-source/build/java/release");
+
+//        String pdfPath = "/home/alvin/yjrwork/bankStreamReco/纯文本类型的银行流水pdf/林翔 东莞农商行 200080002850534.pdf";
+        List<String> allPdfs = new ArrayList<>();
+        String pdfDir = "pdfs";
+        traverse(new File(pdfDir), allPdfs);
+        for (String pdfPath : allPdfs) {
+            final String acceleratorPath = PdfUtils.getAcceleratorPath(pdfPath);
+            Document document;
+            try {
+                if (PdfUtils.acceleratorValid(pdfPath, acceleratorPath)) {
+                    document = Document.openDocument(pdfPath, acceleratorPath);
+                } else {
+                    document = Document.openDocument(pdfPath);
+                }
+                document.saveAccelerator(acceleratorPath);
+                int pageCount = document.countPages();
+                for (int i = 0; i < pageCount; i++){
+                    File output = new File("/home/alvin/opensoureWork/pdf2Img/img"+ File.separator+i+".jpg");
+//            pdfDocument.saveAsPng(i, "/home/alvin/yjrwork/bankStreamReco/pdf转image"+ File.separator+i+".png",
+//                    1.0f, ImageType.IMAGE_TYPE_RGB);
+                    Page page = document.loadPage(i);
+                    final BufferedImage bufferedImage = Transform.turnPage2Img(page);
+                    try {
+                        ImageIO.write(bufferedImage, "jpg", output);
+                    } catch (Exception e) {
+                        System.out.println("路径出错");
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                System.out.println(pdfPath);
+            }
+
+        }
+    }
+}
---
+++ src/main/java/com/alvin/ai/utils/PdfUtils.java
@@ -0,0 +1,38 @@
+package com.alvin.ai.utils;
+
+import com.artifex.mupdf.fitz.Pixmap;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+/**
+ * @ClassName PdfUtils
+ * @Description TODO
+ * @Author Yang Jianren
+ * @Date 2021/12/13 上午10:49
+ * @Version 1.0
+ */
+public class PdfUtils {
+    public static String getAcceleratorPath(String documentPath) {
+        String acceleratorName = documentPath.substring(1);
+        acceleratorName = acceleratorName.replace(File.separatorChar, '%');
+        acceleratorName = acceleratorName.replace('\\', '%');
+        acceleratorName = acceleratorName.replace(':', '%');
+        String tmpdir = System.getProperty("java.io.tmpdir");
+        return new StringBuffer(tmpdir).append(File.separatorChar).append(acceleratorName).append(".accel").toString();
+    }
+
+    public static boolean acceleratorValid(String documentPath, String acceleratorPath) {
+        long documentModified = new File(documentPath).lastModified();
+        long acceleratorModified = new File(acceleratorPath).lastModified();
+        return acceleratorModified != 0 && acceleratorModified > documentModified;
+    }
+
+    public static BufferedImage imageFromPixmap(Pixmap pixmap) {
+        int w = pixmap.getWidth();
+        int h = pixmap.getHeight();
+        BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_3BYTE_BGR);
+        image.setRGB(0, 0, w, h, pixmap.getPixels(), 0, w);
+        return image;
+    }
+}
